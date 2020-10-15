8588e10b9d51ef0c6fb25e718660c9c19db64ef5
Rasmus Knudsen <64476703+rfriisk@users.noreply.github.com>
2020-10-15 10:00:47 +0200

    Add files via upload

---
+++ SimpleTrader/SimpleTrader.EntityFramework/SimpleTraderDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SimpleTrader.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SimpleTrader.EntityFramework {
+    public class SimpleTraderDbContext : DbContext {
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Account> Accounts { get; set; }
+        public DbSet<AssetTransaction> AssetTransactions { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder ) {
+
+            optionsBuilder.UseSqlServer("");
+
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
